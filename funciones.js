114dab3754528590ed5a1bdb7db5fd8a77720856
jeremias <juansebas7011@gmail.com>
2025-01-24 13:14:49 -0500

    Proyecto terminado

--- funciones.js
+++ funciones.js
@@ -1,1 +1,19 @@
- 
+let ButtonValue;
+
+disminuirNumero  = () =>{
+    ButtonValue= parseInt(document.getElementById("number").innerHTML)-1;
+    document.getElementById("number").innerHTML = ButtonValue;
+    return;
+
+}
+restablecerNumero =()=>{
+    document.getElementById("number").innerHTML= 0;
+    
+
+}   
+aumentarNumero =() =>{ 
+    ButtonValue= parseInt(document.getElementById("number").innerHTML)+1;
+    document.getElementById("number").innerHTML = ButtonValue;
+    return;
+
+}
